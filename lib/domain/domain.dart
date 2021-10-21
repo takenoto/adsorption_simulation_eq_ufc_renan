924957df27ea7f1118645c6423f6d34aa7bb39dc
takenoto <mendesrenan5@gmail.com>
2021-10-21 18:45:29 -0300

    created domain library

---
+++ lib/domain/domain.dart
@@ -0,0 +1,7 @@
+library domain;
+
+export 'utils/utils.dart';
+export 'adsorption_colums/adsorption_columns.dart';
+export 'chemistry/chemistry.dart';
+export 'discretization/discretization.dart';
+export 'math/math.dart';
